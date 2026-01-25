e5ddf974a850e0c87b378f340a2ae7681ccc950c
Bhaumik-05 <modibhaumik180@gmail.com>
2026-01-25 14:23:56 +0530

    [IMP] Refactor pollapp views using Django generic class-based views

--- pollapp/views.py
+++ pollapp/views.py
@@ -1,28 +1,28 @@
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from .models import Question
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "pollapp/index.html", context)
+
+class IndexView(generic.ListView):
+    template_name = "pollapp/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "pollapp/detail.html", {
-        "question": question
-    })
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "pollapp/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "pollapp/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "pollapp/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -30,7 +30,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form with an error message
         return render(
             request,
             "pollapp/detail.html",
@@ -42,11 +41,6 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents duplicate submissions.
         return HttpResponseRedirect(
             reverse("pollapp:results", args=(question.id,))
         )
-
-
